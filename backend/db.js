724f7867e84c64be06549503befd1873aa435399
chenaoh <cdhenao@gmial.com>
2026-07-08 23:06:17 -0600

    Cambio en .env

--- backend/db.js
+++ backend/db.js
@@ -1,20 +1,28 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'maxi1407',
-  database: process.env.DB_NAME || 'snackcenter',
-  port: process.env.DB_PORT || 3306
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0
 });
 
-connection.connect((err) => {
+pool.getConnection((err, connection) => {
   if (err) {
     console.error('❌ Error DB:', err);
     return;
   }
+
   console.log('✅ MySQL conectado');
+  connection.release();
 });
 
-module.exports = connection;
+module.exports = pool;
